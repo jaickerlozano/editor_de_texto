befc3c03453181328f633ef03d173eb8e9c592a0
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2024-10-17 02:58:03 -0300

    Añadiendo funcion guardar, guardar_como y mejoras en la funcion abrir

--- editor_texto.py
+++ editor_texto.py
@@ -1,9 +1,7 @@
-import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 from tkinter import filedialog as FileDialog
 
-global ruta # Esta variable nos permitirá almacenar en ella la ruta del fichero
+ruta = "" # Esta variable nos permitirá almacenar en ella la ruta del fichero
 
 def nuevo() -> None:
     'Esta función permite abrir un nuevo fichero'
@@ -15,7 +13,7 @@ def nuevo() -> None:
     root.title("Notepad")
 
 def abrir() -> None:
-    'Esta función permite abrir un fichero'
+    'Esta función permite abrir un fichero existente'
 
     global ruta
     mensaje.set("Abrir fichero")
@@ -31,19 +29,59 @@ def abrir() -> None:
         contenido = fichero.read()
         texto.delete('1.0', END)
         texto.insert('insert', contenido)
+        nombre_archivo = ruta.split("/")[-1] # Tomo solo el nombre del archivo en la ruta 
+        nombre_archivo = nombre_archivo.replace(".txt","") # Elimino el .txt del nombre del archivo
         fichero.close()
-        root.title(ruta + 'Notepad')
+        root.title(f"{nombre_archivo}") # Muestro como título el nombre del archivo abierto
+
+def guardar() -> None:
+    'Esta función permite guardar un fichero'
+
+    global ruta
+    mensaje.set("Guardar fichero")
+    if ruta != "":
+        contenido = texto.get(1.0,'end-1c')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set("Fichero guardado correctamente")
+    else:
+        guardar_como()
+
+def guardar_como() -> None:
+    'Esta función permite guardar un fichero'
+
+    global ruta
+    mensaje.set("Guardar fichero como")
+    
+    fichero = FileDialog.asksaveasfile(title="Guardar fichero",
+        mode="w", defaultextension=".txt")
+
+    if fichero is not None:
+        ruta = fichero.name
+        contenido = texto.get(1.0,'end-1c')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set("Fichero guardado correctamente")
+
+    else:
+        mensaje.set("Guardado cancelado")
+        ruta = ""
+
 
 # Ventana principal
 root = Tk()
-root.title("Notepad")
+root.title("Mi Editor de Texto")
 root.geometry("800x500")
 
 # Barra de menú
-menubar = Menu(root) # Se instancia la barra menú
+menubar = Menu(root) # Se instancia la barra Menú
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="Nuevo", command=nuevo)
 filemenu.add_command(label="Abrir", command=abrir)
+filemenu.add_command(label="Guardar", command=guardar)
+filemenu.add_command(label="Guardar como...", command=guardar_como)
 filemenu.add_command(label="Salir", command=root.quit)
 menubar.add_cascade(menu=filemenu, label="Archivo") 
 
